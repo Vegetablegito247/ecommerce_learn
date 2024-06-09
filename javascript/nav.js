b3f791e75bd378fdd25cb7182c9bb67a0f1167ca
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-06-09 14:51:36 -0600

    first commit

---
+++ javascript/nav.js
@@ -0,0 +1,12 @@
+let burger = document.querySelector('.burger');
+let sidemenu = document.querySelector('.sidemenu');
+
+burger.addEventListener('click', () => {
+    if (sidemenu.style.bottom == '-100vh') {
+        sidemenu.style.transition = 'bottom .7s'
+        sidemenu.style.bottom = 0
+    }
+    else {
+        sidemenu.style.bottom = '-100vh'
+    }
+})
